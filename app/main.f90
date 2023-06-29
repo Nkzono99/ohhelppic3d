82c83c3942f6dd97a36e8173bdfde010583497a5
Nkzono99 <210x218x@stu.kobe-u.ac.jp>
2023-06-29 15:07:24 +0900

    Init: Fpm initialization.

---
+++ app/main.f90
@@ -0,0 +1,5 @@
+program main
+  implicit none
+
+  print *, "hello from project ohhelppic3d"
+end program main
---
+++ src/ohhelppic3d.f90
@@ -0,0 +1,10 @@
+module ohhelppic3d
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, ohhelppic3d!"
+  end subroutine say_hello
+end module ohhelppic3d
