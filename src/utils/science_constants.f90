56e80445d4199afddce37fa05f17f0b709ec9546
Nkzono99 <210x218x@stu.kobe-u.ac.jp>
2023-07-06 04:08:39 +0900

    Add: Added scientific constant module.

---
+++ src/utils/science_constants.f90
@@ -0,0 +1,8 @@
+module m_science_constants
+    implicit none
+
+    double precision, parameter :: pi = 3.14159265358979d0
+    double precision, parameter :: elemental_charge = 1.602176634d-19
+    double precision, parameter :: electron_mass = 9.1093837015d-31
+
+end module
