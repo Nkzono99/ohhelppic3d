19e253677e4c6c8db23e8d56f05da006d9558f4b
Nkzono99 <210x218x@stu.kobe-u.ac.jp>
2023-07-08 05:49:12 +0900

    Change: Chaged fft interface.

--- src/fft/fftw3/mpi_fftw3_solver.f90
+++ src/fft/fftw3/mpi_fftw3_solver.f90
@@ -3,7 +3,8 @@ module m_mpi_fftw_solver
     use m_fftw3_mpi
     use m_fft_boundary_type
     use m_block
-    use m_mpi_block_rebase
+    use m_mpi_block_rebaser
+    use mpi
     implicit none
 
     private
@@ -13,9 +14,12 @@ module m_mpi_fftw_solver
     type, extends(t_MPIFFTSolver3d) :: t_MPIFFTWSolver3d
         type(C_PTR) :: forward_plan
         type(C_PTR) :: backward_plan
+        double precision :: normalizers(3)
 
-        type(t_Block) :: local_block
         type(t_Block) :: require_block
+
+        integer :: myid
+        integer :: nprocs
 
         integer :: communicator
         integer :: tag
@@ -23,8 +27,8 @@ module m_mpi_fftw_solver
         type(C_PTR) :: ptr_fft_array
         real(C_DOUBLE), pointer :: fft_array(:, :, :)
 
-        type(t_MPIBlockRebasor) :: in2fftarray
-        type(t_MPIBlockRebasor) :: fftarray2out
+        type(t_MPIBlockRebaser) :: in2fftarray
+        type(t_MPIBlockRebaser) :: fftarray2out
     contains
         procedure :: forward => mpiFFTWSolver_forward
         procedure :: backward => mpiFFTWSolver_backward
@@ -44,52 +48,65 @@ contains
         case (FFT_BoundaryType_Periodic)
             forward_fft_type = FFTW_R2HC
             backward_fft_type = FFTW_HC2R
+            normalizer = n
 
         case (FFT_BoundaryType_Dirichlet)
             forward_fft_type = FFTW_RODFT00
             backward_fft_type = FFTW_RODFT00
+            normalizer = 2.0d0*(n + 1.0d0)
 
         case (FFT_BoundaryType_Neumann)
             forward_fft_type = FFTW_REDFT00
             backward_fft_type = FFTW_REDFT00
+            normalizer = 2.0d0*(n - 1.0d0)
 
         case (FFT_BoundaryType_Dirichlet_Neumann)
             forward_fft_type = FFTW_RODFT01
             backward_fft_type = FFTW_RODFT10
+            normalizer = 2.0d0*n
 
         case (FFT_BoundaryType_Neumann_Dirichlet)
             forward_fft_type = FFTW_REDFT01
             backward_fft_type = FFTW_REDFT10
+            normalizer = 2.0d0*n
         end select
     end subroutine
 
     function new_MPIFFTWSolver3d(fft_boundary_types, &
                                  local_block, &
-                                 nx, ny, nz, &
+                                 global_block, &
                                  myid, nprocs, &
                                  communicator, tag) result(obj)
         integer, intent(in) :: fft_boundary_types(3)
         !> Block that the local process has.
         !> global index 座標系は1:nx
         type(t_Block), intent(in) :: local_block
-        integer, intent(in) :: nx, ny, nz
+        type(t_Block), intent(in) :: global_block
         integer, intent(in) :: myid
         integer, intent(in) :: nprocs
         integer, intent(in) :: communicator
         integer, intent(in) :: tag
         type(t_MPIFFTWSolver3d) :: obj
 
+        integer :: nx, ny, nz
         integer(kind=C_INTPTR_T) :: iptr_nx, iptr_ny, iptr_nz
         integer(C_INTPTR_T) :: local_nz, local_z_start
+
+        obj%boundary_types(:) = fft_boundary_types(:)
+        obj%local_block = local_block
+        obj%global_block = global_block
+        obj%myid = myid
+        obj%nprocs = nprocs
+        obj%communicator = communicator
+        obj%tag = tag
+
+        nx = global_block%sizes(1)
+        ny = global_block%sizes(2)
+        nz = global_block%sizes(3)
 
         iptr_nx = nx
         iptr_ny = ny
         iptr_nz = nz
-
-        obj%boundary_types(:) = fft_boundary_types(:)
-        obj%local_block = local_block
-        obj%communicator = communicator
-        obj%tag = tag
 
         call fftw_mpi_init()
 
@@ -124,6 +141,8 @@ contains
             call convert_fft_type(nz, fft_boundary_types(1), &
                                   forward_fft_type_z, backward_fft_type_z, normalizer_z)
 
+            obj%normalizers = [normalizer_x, normalizer_y, normalizer_z]
+
             obj%forward_plan = fftw_mpi_plan_r2r_3d(iptr_nz, iptr_ny, iptr_nx, &
                                                     obj%fft_array, obj%fft_array, &
                                                     obj%communicator, &
@@ -144,9 +163,11 @@ contains
         block ! Set the coordinate range (block) to be used in fftw.
             integer :: zs, ze
 
-            zs = local_z_start
-            ze = local_z_start + local_nz-1
-            obj%require_block = new_Block([1, 1, zs], [int(nx), int(ny), ze])
+            ! local_z from fftw start at 0 to nz-1
+            zs = local_z_start + global_block%start(3)
+            ze = local_z_start + global_block%start(3) + local_nz - 1
+            obj%require_block = new_Block([global_block%start(1), global_block%start(2), zs], &
+                                          [global_block%end(1), global_block%end(2), ze])
         end block
 
         block ! Create a converter between the coordinate range used in fft and the coordinate range for subdomain.
@@ -157,33 +178,41 @@ contains
                 pids(i) = i - 1
             end do
 
-            obj%in2fftarray = new_MPIBlockRebasor(obj%local_block, obj%require_block, pids, myid, obj%communicator)
-            obj%fftarray2out = new_MPIBlockRebasor(obj%require_block, obj%local_block, pids, myid, obj%communicator)
+            obj%in2fftarray = new_MPIBlockRebaser(obj%local_block, obj%require_block, pids, myid + 1, obj%communicator)
+            obj%fftarray2out = new_MPIBlockRebaser(obj%require_block, obj%local_block, pids, myid + 1, obj%communicator)
         end block
     end function
 
     subroutine mpiFFTWSolver_forward(self, in, out)
         class(t_MPIFFTWSolver3d), intent(inout) :: self
-        double precision :: in(:, :, :)
-        double precision :: out(:, :, :)
+        double precision, intent(inout) :: in(self%local_block%start(1):self%local_block%end(1), &
+                                              self%local_block%start(2):self%local_block%end(2), &
+                                              self%local_block%start(3):self%local_block%end(3))
+        double precision, intent(inout) :: out(self%local_block%start(1):self%local_block%end(1), &
+                                               self%local_block%start(2):self%local_block%end(2), &
+                                               self%local_block%start(3):self%local_block%end(3))
 
-        call self%in2fftarray%rebase(in, self%fft_array, tag=self%tag)
-
+        call self%in2fftarray%rebase(in, self%fft_array(:, :, :), tag=self%tag)
         call fftw_mpi_execute_r2r(self%forward_plan, self%fft_array, self%fft_array)
-
-        call self%fftarray2out%rebase(self%fft_array, out, tag=self%tag)
+        call self%fftarray2out%rebase(self%fft_array(:, :, :), out, tag=self%tag)
     end subroutine
 
     subroutine mpiFFTWSolver_backward(self, in, out)
         class(t_MPIFFTWSolver3d), intent(inout) :: self
-        double precision :: in(:, :, :)
-        double precision :: out(:, :, :)
+        double precision, intent(inout) :: in(self%local_block%start(1):self%local_block%end(1), &
+                                              self%local_block%start(2):self%local_block%end(2), &
+                                              self%local_block%start(3):self%local_block%end(3))
+        double precision, intent(inout) :: out(self%local_block%start(1):self%local_block%end(1), &
+                                               self%local_block%start(2):self%local_block%end(2), &
+                                               self%local_block%start(3):self%local_block%end(3))
 
         call self%in2fftarray%rebase(in, self%fft_array, tag=self%tag)
 
         call fftw_mpi_execute_r2r(self%backward_plan, self%fft_array, self%fft_array)
 
         call self%fftarray2out%rebase(self%fft_array, out, tag=self%tag)
+
+        out = out/product(self%normalizers)
     end subroutine
 
     subroutine mpiFFTWSolver_cleanup(self)
